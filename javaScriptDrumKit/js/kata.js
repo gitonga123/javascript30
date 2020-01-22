e4400dec21b3afa2d7e066879f03b1449ca7a0d0
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2020-01-22 09:12:06 +0300

    Using switch for this solution

--- javaScriptDrumKit/js/kata.js
+++ javaScriptDrumKit/js/kata.js
@@ -1,6 +1,6 @@
 // You probably know the "like" system from Facebook and other pages.People can "like" blog posts, pictures or other items.We want to create the text that should be displayed next to such an item.
 
-function likes(names) {
+function likesy(names) {
     let message = '';
     if (names.length === 0) {
         message = "no one likes this";
@@ -23,6 +23,19 @@ function likes(names) {
     }
 
     return message;
+}
+
+// solution 2
+
+function likes(names) {
+    names = names || [];
+    switch (names.length) {
+        case 0: return 'no one likes this'; break;
+        case 1: return names[0] + ' likes this'; break;
+        case 2: return names[0] + ' and ' + names[1] + ' like this'; break;
+        case 3: return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this'; break;
+        default: return names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others like this';
+    }
 }
 
 // console.log(likes([])); // must be "no one likes this"
