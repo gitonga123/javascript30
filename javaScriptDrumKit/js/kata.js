6b740d87b1bae55a06f0e1dbf41fe10f0fa63b11
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2020-01-22 11:47:52 +0300

    Kata 1 refactored

--- javaScriptDrumKit/js/kata.js
+++ javaScriptDrumKit/js/kata.js
@@ -41,5 +41,5 @@ function likes(names) {
 // console.log(likes([])); // must be "no one likes this"
 // console.log(likes(["Peter"])); // must be "Peter likes this"
 // console.log(likes(["Jacob", "Alex"])); // must be "Jacob and Alex like this"
-console.log(likes(["Max", "John", "Mark"]));// must be "Max, John and Mark like this"
+// console.log(likes(["Max", "John", "Mark"]));// must be "Max, John and Mark like this"
 //console.log(likes(["Alex", "Jacob", "Mark", "Max"])); // must be "Alex, Jacob and 2 others like this"
